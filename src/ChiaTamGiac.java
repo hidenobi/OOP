7aae52579bb2ccb3228d9478fb406fd1a8dde005
Tuan Vo <dungtuan095@gmail.com>
2022-08-18 09:38:25 +0700

    First commit

---
+++ src/ChiaTamGiac.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ChiaTamGiac {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        for(int i=0;i<t;i++){
+            int n = sc.nextInt();
+            int h = sc.nextInt();
+            for(int j=1;j<=n-1;j++){
+                System.out.printf("%.6f ",h*Math.sqrt((double) j/n));
+            }
+            System.out.println();
+        }
+    }
+}
